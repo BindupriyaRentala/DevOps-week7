f994d65f38442f6f8f85075f348aa13c80391133
BindupriyaRentala <rentalabindupriya@gmail.com>
2026-08-24 10:23:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def register():
     year=request.form['year']
     return render_template('successful.html',name=name,year=year)
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
